7af273ad73124c4b4b04721bdde4655cd59f8d5f
Zuzev Rodion <Knoty01@gmail.com>
2016-07-09 19:05:26 +0300

    added name generation from syllables

--- RandomNameGenerator.php
+++ RandomNameGenerator.php
@@ -3,10 +3,18 @@ require_once ('NameGenerator.php');
 
 class RandomNameGenerator implements NameGenerator
 {
+    /** @var string[] */
+    private $syllables = ['nar', 'ka', 'wer', 'tok', 'far', 'ta', 'to', 'ko', 'tor', 'tak', 'kon', 'ro', 'de', 'kar', 'man', 'ga', 'rus', 'lan'];
 
     /** @return string */
     function getName()
     {
-        return substr(str_shuffle("abcdefghijklmnopqrstuvwxyz"), 0, 9);
+        $syllables_count = mt_rand(2, 3);
+        $last_index = count($this->syllables) - 1;
+        $name = "";
+        for ($i = 0; $i < $syllables_count; ++$i) {
+            $name = $name . $this->syllables[mt_rand(0, $last_index)];
+        }
+        return $name;
     }
 }
--- RandomNameGeneratorTest.php
+++ RandomNameGeneratorTest.php
@@ -9,4 +9,20 @@ class RandomNameGeneratorTest extends PHPUnit_Framework_TestCase
         $name = $generator->getName();
         $this->assertInternalType('string', $name);
     }
+
+    /** @test */
+    function minMaxNameLength()
+    {
+        $generator = new RandomNameGenerator();
+        $names = [];
+
+        for ($i=0; $i < 1000000; ++$i)
+            $names[$i] = $generator->getName();
+
+        $name_lengths = array_map("strlen", $names);
+        $min_name_length = min($name_lengths);
+        $max_name_length = max($name_lengths);
+        $this->assertEquals(4, $min_name_length);
+        $this->assertEquals(9, $max_name_length);
+    }
 }
